b33b91c573abdff2f75d288ca0b79589256bb34d
Alexander Munko <seasle98@gmail.com>
2023-12-20 12:30:23 +0700

    Добавляет сборку для GitHub Pages

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,4 +1,11 @@
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
+
 module.exports = (config) => {
+  config.addPlugin(EleventyHtmlBasePlugin, {
+    baseHref:
+      process.env.NODE_ENV === 'production' ? '/multipage-website/' : '/',
+  });
+
   config.addPassthroughCopy({
     'node_modules/@fontsource/roboto/files': 'styles/files',
     'src/assets': 'assets',
